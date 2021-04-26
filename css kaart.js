7261c53e11b4d401439e40a4864693f7a8a7e365
Merijnvdven <458672@student.fontys.nl>
2021-04-26 14:50:22 +0200

    10

---
+++ css kaart.js
@@ -0,0 +1,62 @@
+[
+    {
+      "picture": "http://placehold.it/32x32",
+      "age": 32,
+      "eyeColor": "blue",
+      "name": "Jami Petty",
+      "gender": "female",
+      "company": "EDECINE",
+      "email": "jamipetty@edecine.com",
+      "phone": "+1 (957) 496-2071",
+      "address": "604 Village Road, Greenfields, Hawaii, 6197",
+      "about": "Eu ad amet nulla dolore fugiat sunt aliquip deserunt proident ut excepteur cillum reprehenderit magna. Ut id et voluptate magna fugiat aliqua commodo exercitation labore ea veniam in. Eiusmod occaecat ullamco labore aliquip sit et irure. Aliqua dolore do amet do ut dolore nulla pariatur non est nostrud fugiat. Occaecat adipisicing quis ullamco esse proident laborum sint cupidatat veniam ullamco ea quis.\r\n"
+    },
+    {
+      "picture": "http://placehold.it/32x32",
+      "age": 26,
+      "eyeColor": "brown",
+      "name": "Jeanne Wheeler",
+      "gender": "female",
+      "company": "CODAX",
+      "email": "jeannewheeler@codax.com",
+      "phone": "+1 (879) 452-3465",
+      "address": "231 Arlington Avenue, Deputy, Vermont, 9485",
+      "about": "Anim ex mollit pariatur veniam laborum Lorem esse minim anim anim duis quis cillum. Pariatur officia proident nostrud sunt id exercitation quis aute adipisicing. Nostrud qui dolor sint et sint ex occaecat cupidatat aute incididunt consequat. Sint exercitation Lorem dolore aute sint laboris. Anim sit cillum cillum ipsum sit esse mollit eu non non irure consequat irure.\r\n"
+    },
+    {
+      "picture": "http://placehold.it/32x32",
+      "age": 29,
+      "eyeColor": "green",
+      "name": "Floyd Simon",
+      "gender": "male",
+      "company": "BULLJUICE",
+      "email": "floydsimon@bulljuice.com",
+      "phone": "+1 (988) 582-3898",
+      "address": "966 Eldert Street, Diaperville, California, 3213",
+      "about": "Duis enim ut officia nostrud consectetur ad culpa consequat nisi aliqua in. Occaecat sunt commodo pariatur enim. Aliqua quis cillum amet nisi ad proident dolore consectetur do pariatur. Nisi officia commodo exercitation sunt in magna. Magna labore est duis velit amet laborum proident commodo adipisicing tempor consectetur culpa. Incididunt amet proident reprehenderit excepteur velit fugiat sint excepteur culpa amet et laborum cupidatat.\r\n"
+    },
+    {
+      "picture": "http://placehold.it/32x32",
+      "age": 28,
+      "eyeColor": "blue",
+      "name": "Howell Armstrong",
+      "gender": "male",
+      "company": "MARKETOID",
+      "email": "howellarmstrong@marketoid.com",
+      "phone": "+1 (832) 472-3760",
+      "address": "653 Jay Street, Mammoth, Mississippi, 6198",
+      "about": "Aliqua aute reprehenderit voluptate mollit irure sunt esse elit nostrud consectetur culpa ullamco et. Deserunt proident pariatur officia officia duis cupidatat ad sunt anim. Do qui voluptate culpa enim pariatur mollit ipsum voluptate elit deserunt velit id. Consequat qui consectetur proident nisi incididunt laboris commodo reprehenderit ea ut incididunt duis.\r\n"
+    },
+    {
+      "picture": "http://placehold.it/32x32",
+      "age": 38,
+      "eyeColor": "blue",
+      "name": "Yang Fernandez",
+      "gender": "male",
+      "company": "BIZMATIC",
+      "email": "yangfernandez@bizmatic.com",
+      "phone": "+1 (949) 482-3621",
+      "address": "368 Brigham Street, Gwynn, Pennsylvania, 3508",
+      "about": "Aliqua occaecat tempor aute eiusmod dolore aute occaecat proident commodo. Eu proident cupidatat quis minim velit in labore exercitation voluptate exercitation ea. Consequat veniam eiusmod aliqua veniam excepteur veniam anim. Laborum fugiat minim tempor non adipisicing irure eu aliqua velit ad adipisicing enim.\r\n"
+    }
+  ]
