d1148bfd6f56c58761273b8ba3b5172ef9d5d9e7
Merijnvdven <458672@student.fontys.nl>
2021-06-03 13:58:03 +0200

    3-6

---
+++ advanced development/anime.js
@@ -0,0 +1,11 @@
+
+
+anime({
+    targets: '.youtube path',
+    strokeDashoffset: [anime.setDashoffset, 0],
+    easing: 'easeInOutSine',
+    duration: 1500,
+    direction: 'alternate',
+    loop: true
+})
+
